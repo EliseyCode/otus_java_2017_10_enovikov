bb3eb60c8602958644160b2af6db44972ef55a05
Elisey Novikov <enovikow6@gmail.com>
2017-10-17 02:24:19 +0300

    add maven dependencies and XUtils class for google guava

--- hw1/src/main/java/ru/enovikow/Main.java
+++
@@ -1,4 +0,0 @@
-package ru.enovikow;
-
-public class Main {
-}
---
+++ hw1/src/main/java/ru/enovikow/otus/Main.java
@@ -0,0 +1,15 @@
+package ru.enovikow;
+
+import ru.enovikow.otus.utils.XUtils;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        System.out.println(list);
+        List<Integer> newList = XUtils.reverseList(list);
+        System.out.println(newList);
+    }
+}
