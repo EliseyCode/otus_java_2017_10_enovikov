3939128aa15fe7bcf1f93aa99052c80e0099edd1
Elisey Novikov <enovikow6@gmail.com>
2017-10-17 02:22:17 +0300

    add maven dependencies and XUtils class for google guava

--- hw1/src/main/java/ru/enovikow/otus/utils/XUtils.java
+++ hw1/src/main/java/ru/enovikow/otus/utils/XUtils.java
@@ -1,15 +1,4 @@
 package ru.enovikow.otus.utils;
 
-import com.google.common.collect.ImmutableList;
-import java.util.List;
-
-public class ListReverse {
-
-//    public List<Integer> reverseList() {
-//
-//    }
-
-    public List<Integer> reverseList(List<Integer> list) {
-        return (List) ImmutableList.of(list).reverse();
-    }
+public class XUtils {
 }
