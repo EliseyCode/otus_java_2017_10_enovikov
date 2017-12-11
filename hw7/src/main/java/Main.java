ed4b25fe8160fce68770b9722ca1b2dc820ea571
Elisey Novikov <enovikow6@gmail.com>
2017-12-11 21:34:21 +0300

    with single ATM

--- hw7/src/main/java/Main.java
+++ hw7/src/main/java/Main.java
@@ -1,5 +1,23 @@
+import atm.ATM;
+import atm.Cell;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        List<Cell> cells = new ArrayList<>();
 
+        cells.add(new Cell(1, 10));
+        cells.add(new Cell(5, 10));
+        cells.add(new Cell(10, 10));
+        cells.add(new Cell(50, 10));
+        cells.add(new Cell(100, 10));
+
+        ATM atm = new ATM(cells);
+        System.out.println("Initial balance: " + atm.getBalance());
+
+        atm.withdraw(1578);
+        System.out.println("Final balance: " + atm.getBalance());
     }
 }
