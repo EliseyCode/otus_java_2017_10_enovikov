36f70477784fe86443e30fc5b266fe092e1b27ed
Elisey Novikov <enovikow6@gmail.com>
2018-01-14 13:58:44 +0300

    hw8 Json writer review improvements

--- hw8/src/main/java/JsonWriter.java
+++ hw8/src/main/java/JsonWriter.java
@@ -1,8 +1,6 @@
 import java.lang.reflect.Array;
 import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 public class JsonWriter {
 
@@ -15,25 +13,26 @@ public class JsonWriter {
             return sb.append("").toString();
         } else if (serializeFieldValue(jsonObject).equals(OBJECT)) {
             sb.append("{");
-            Field[] fields = jsonObject.getClass().getDeclaredFields();
-            for (Field field : fields) {
+            Iterator<Field> iter = Arrays.asList(jsonObject.getClass().getDeclaredFields()).iterator();
+            while (iter.hasNext()) {
+                Field field = iter.next();
                 field.setAccessible(true);
+
                 if (sb.toString().endsWith("}")) {
                     sb.append(",\"").append(field.getName()).append("\":");
                 } else {
                     sb.append("\"").append(field.getName()).append("\":");
                 }
                 sb.append(serializeJSON(field.get(jsonObject)));
+
+
+                if (iter.hasNext()) { sb.append(","); }
+                else { sb.append("}"); }
             }
-
-            if (sb.toString().endsWith(",")) {
-                sb.deleteCharAt(sb.length() - 1).append("}");
-            } else sb.append("}");
-        } else sb.append(serializeFieldValue(jsonObject));
-
-        if (sb.toString().contains(",]")) {
-            sb.deleteCharAt(sb.length() - 3);
+        } else {
+            sb.append(serializeFieldValue(jsonObject));
         }
+
         return sb.toString();
     }
 
@@ -49,11 +48,11 @@ public class JsonWriter {
     }
 
     private String serializeString(Object jsonObject) throws IllegalAccessException {
-        return "\"" + jsonObject.toString() + "\",";
+        return "\"" + jsonObject.toString() + "\"";// "\",";
     }
 
     private String serializeNumberOrBoolean(Object jsonObject) throws IllegalAccessException {
-        return jsonObject.toString() + ",";
+        return jsonObject.toString();// + ",";
     }
 
     private String serializeCollection(Object jsonObject) throws IllegalAccessException {
@@ -69,10 +68,16 @@ public class JsonWriter {
             }
         }
 
-        for (Object element : list) {
-            sb.append(serializeFieldValue(element));
+        Iterator<Object> iter = list.iterator();
+
+        while (iter.hasNext()) {
+            sb.append(serializeFieldValue(iter.next()));
+            if (iter.hasNext()) {
+                sb.append(",");
+            }
         }
-        sb.append("],");
+        sb.append("]");
+
         return sb.toString();
     }
 }
