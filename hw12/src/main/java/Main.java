05f383205597c9954024b57ddc3bfa07e8d18a88
Elisey <enovikow6@gmail.com>
2018-01-29 03:13:06 +0300

    hw12 init

--- hw12/src/main/java/Main.java
+++ hw12/src/main/java/Main.java
@@ -1,5 +1,11 @@
 import cache.CacheEngineImpl;
 import database.DBServiceImpl;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import server.AdminServlet;
 import user_data.AddressDataSet;
 import user_data.DataSet;
 import user_data.UserDataSet;
@@ -7,9 +13,13 @@ import user_data.UserDataSet;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
+    private final static int PORT = 8090;
+    private final static String PUBLIC_HTML = "public_html";
 
-        CacheEngineImpl<Long, DataSet> cacheEngine = new CacheEngineImpl<>(1, 700, 700, false);
+    public static void main(String[] args) throws Exception {
+
+
+        CacheEngineImpl<Long, DataSet> cacheEngine = new CacheEngineImpl<>(1, 700, 0, true);
 
         DBServiceImpl dbService = new DBServiceImpl(cacheEngine);
 
@@ -19,20 +29,39 @@ public class Main {
 
         UserDataSet userFromDB;
         List<UserDataSet> list;
+        List<UserDataSet> secondList;
 
         String status = dbService.getStatus();
         System.out.println("Status: " + status);
         dbService.save(user1);
         dbService.save(user2);
         dbService.save(user3);
+
         System.out.println("User with name Elisey: " + dbService.getByName("Elisey"));
+
+
         list = dbService.getAllUsers();
+        secondList = dbService.getAllUsers();
         userFromDB = dbService.load(3);
 
         list.forEach(user -> System.out.println("User from List " + user.toString()));
+        secondList.forEach(user -> System.out.println("User from List " + user.toString()));
 
         System.out.println("User from DB: " + userFromDB.toString());
 
         dbService.close();
+
+
+        ResourceHandler resourceHandler = new ResourceHandler();
+        resourceHandler.setResourceBase(PUBLIC_HTML);
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.addServlet(new ServletHolder(new AdminServlet(cacheEngine)), "/admin");
+
+        Server server = new Server(PORT);
+        server.setHandler(new HandlerList(resourceHandler, context));
+
+        server.start();
+        server.join();
     }
 }
---
+++ hw12/src/main/java/server/AdminServlet.java
@@ -0,0 +1,55 @@
+package server;
+
+import cache.CacheEngineImpl;
+import user_data.DataSet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class AdminServlet extends HttpServlet {
+    public static final String LOGIN_PARAMETER_NAME = "login";
+    public static final String PASSWORD_PARAMETER_NAME = "password";
+
+    private static final String LOGIN_VARIABLE_NAME = "log";
+    private static final String PASSWORD_VARIABLE_NAME = "pass";
+
+    private static final String ADMIN_PAGE_SERVLET = "admin.html";
+    private static final String CACHE_PAGE_SERVLET = "cache.html";
+
+    private final CacheEngineImpl<Long, DataSet> cacheEngine;
+
+
+    public AdminServlet(CacheEngineImpl<Long, DataSet> cacheEngine) {
+        this.cacheEngine = cacheEngine;
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Map<String, Object> variables = new HashMap();
+        String requestLogin = req.getParameter(LOGIN_PARAMETER_NAME);
+        String requestPassword = req.getParameter(PASSWORD_PARAMETER_NAME);
+
+        if (LOGIN_VARIABLE_NAME.equals(requestLogin) && PASSWORD_VARIABLE_NAME.equals(requestPassword)) {
+            variables.put("hitCount", cacheEngine.getHitCount());
+            variables.put("missCount", cacheEngine.getMissCount());
+
+            resp.getWriter().println(Processor.instance().getPage(CACHE_PAGE_SERVLET, variables));
+            resp.setContentType("text/html;charset=utf-8");
+            resp.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            resp.getWriter().println(Processor.instance().getPage(ADMIN_PAGE_SERVLET, variables));
+            resp.setContentType("text/html;charset=utf-8");
+            resp.setStatus(HttpServletResponse.SC_OK);
+        }
+    }
+}
